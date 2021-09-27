af3210b43a18b3fde7a60d425fa66c80eca1e85e
dpbam <derekphtpt@gmail.com>
2021-09-26 18:13:00 -0600

    project images and descriptions and links working, need to figure out background

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,9 @@ import '../assets/css/style2.css';
 import runbuddy from '../assets/images/runbuddy.png';
 import drinking from '../assets/images/drinking.png';
 import shakeonit from '../assets/images/shakeonit.png';
-
-import { Link } from 'react-router-dom';
+import weather from '../assets/images/weather.png';
+import budget from '../assets/images/budget.png';
+import notetaker from '../assets/images/notetaker.png';
 
 const projectsArray = [
   {
@@ -27,22 +28,24 @@ const projectsArray = [
     link: 'https://nameless-taiga-03231.herokuapp.com/',
   },
   {
-    name: '4th project',
-    image: '',
+    name: 'Whether or Not Weather App',
+    image: weather,
     description: 'this is a description of 4th project',
-    link: '',
+    link: 'https://dpbam.github.io/whether-or-not-weather-app/',
   },
   {
-    name: '5th project',
-    image: '',
-    description: 'this is a description of 5th project',
-    link: '',
+    name: 'Budget Tracker 2000',
+    image: budget,
+    description:
+      'The Budget Tracker 2000 tracks your budget and gives you access to your information at any time. This app works online and offline.',
+    link: 'https://evening-springs-33870.herokuapp.com/',
   },
   {
-    name: '6th project',
-    image: '',
-    description: 'this is a description of 6th project',
-    link: '',
+    name: 'Note Taker 2000',
+    image: notetaker,
+    description:
+      'NOTE TAKER 2000 is an application that writes and saves notes. After the user clicks Get Started on the welcome page it takes them to a second page where it welcomes user input for notes consisting of a title and comments and saves it forever, which is a really long time. This app uses JSON, fs, JavaScript, Express, Node.js, HTML, CSS, and API routes.',
+    link: 'https://obscure-cliffs-88529.herokuapp.com',
   },
 ];
 
@@ -61,16 +64,18 @@ const Project = (p) => {
   console.log(p.image);
   return (
     <div key={p.name} className='projects'>
-      <img
-        src={p.image}
-        alt={p.name}
-        style={{ width: '50vw', height: '50vh' }}
-      ></img>
-      <h3>{p.name}</h3>
-      <p>{p.description}</p>
-      <li>
-        <Link to={p.link}>{p.name}</Link>
-      </li>
+      <div style={{ paddingLeft: '20px', paddingTop: '100px' }}>
+        <img
+          src={p.image}
+          alt={p.name}
+          style={{ width: '50vw', height: '50vh' }}
+        ></img>
+      </div>
+      <div style={{ paddingLeft: '20px', paddingTop: '100px' }}>
+        <h3>{p.name}</h3>
+        <p>{p.description}</p>
+        <a href={p.link}>{p.name} link</a>
+      </div>
     </div>
   );
 };
