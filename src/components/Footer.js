fe771eeb343f774becfe9db7d1337cf173aa470e
dpbam <derekphtpt@gmail.com>
2021-09-27 15:23:26 -0600

    added link to stack overflow in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,22 +3,39 @@ import {
   FaGithub,
   FaRegCopyright,
   FaLinkedin,
-  FaTwitter,
+  FaStackOverflow,
 } from 'react-icons/fa';
 
 export const Footer = () => {
   return (
     <div className='footer' style={{ color: 'white' }}>
       <h1>
-        <FaGithub />
+        <a
+          href='https://github.com/dpbam'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          <FaGithub />
+        </a>
       </h1>
       <h1>
-        <FaLinkedin />
+        <a
+          href='https://www.linkedin.com/in/derek-phelps-76923311/'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          <FaLinkedin />
+        </a>
       </h1>
       <h1>
-        <FaTwitter />
+        <a
+          href='https://stackoverflow.com/users/15781081/dpbam'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          <FaStackOverflow />
+        </a>
       </h1>
-
       <h1>
         <FaRegCopyright />
         Derek Phelps
