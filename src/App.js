9073bee2165375eaacd74683635408fed71615c8
dpbam <derekphtpt@gmail.com>
2021-09-26 19:51:11 -0600

    started the sticky footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Nav from './components/Nav';
 import About from './components/About';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
+import Footer from './components/Footer';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -50,6 +51,7 @@ function App() {
         currentProject={currentProject}
       ></Projects>
       <Contact></Contact> */}
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export const Footer = () => {
-  return <div></div>;
+  return (
+    <div className='footer' style={{ color: 'white' }}>
+      <p>This is some content in the sticky footer</p>
+    </div>
+  );
 };
+
+export default Footer;
