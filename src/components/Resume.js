8de68beca66d210f88cf4fc44a82864349b2125f
dpbam <derekphtpt@gmail.com>
2021-10-03 18:03:17 -0600

    margin on about paragraph fixed

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,11 @@ import React from 'react';
 export const Resume = () => {
   return (
     <div className='resume-link'>
-      <a href='https://docs.google.com/document/d/1BH27QfeoWfPoAIk6aPPAecG3LqL1Tp3unJB9NlP8eZg/edit#'>
+      <a
+        href='https://docs.google.com/document/d/15QOVJsyavSI0D1zb2tNnkKySsDkZtjuQt3MVJDSSjO8/edit?usp=sharing'
+        target='_blank'
+        rel='noreferrer noopener'
+      >
         Let's see the resume.
       </a>
     </div>
