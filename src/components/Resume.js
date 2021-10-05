5e020c82343dda86c1c90ab90b742514a6a44862
dpbam <derekphtpt@gmail.com>
2021-10-05 15:17:12 -0600

    added pointers to resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,6 +3,7 @@ import React from 'react';
 export const Resume = () => {
   return (
     <div className='resume-link'>
+      <h2>👇</h2>
       <a
         href='https://docs.google.com/document/d/15QOVJsyavSI0D1zb2tNnkKySsDkZtjuQt3MVJDSSjO8/edit?usp=sharing'
         target='_blank'
@@ -10,6 +11,7 @@ export const Resume = () => {
       >
         Let's see the resume.
       </a>
+      <h2>👆</h2>
     </div>
   );
 };
