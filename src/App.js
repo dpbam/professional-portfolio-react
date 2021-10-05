877c4d87182194602999b502d47eeff8f0c53e68
dpbam <derekphtpt@gmail.com>
2021-10-04 20:55:43 -0600

    fixed hover on icons in Footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { useState } from 'react';
 import './assets/css/style2.css';
 import background from './assets/images/lazarescu-alexandra-QKXHNLNgdAw-unsplash.jpg';
-// import Header from './components/Header';
 import Nav from './components/Nav';
 import About from './components/About';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import Resume from './components/Resume';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   const [currentComponent, setCurrentComponent] = useState('About');
