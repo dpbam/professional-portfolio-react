d5e4d85c81fec13ef1a232e78b6d9e442a4aeae5
dpbam <derekphtpt@gmail.com>
2021-10-11 16:04:13 -0600

    fixed about paragraph format and background on smaller screens

--- src/components/About.js
+++ src/components/About.js
@@ -53,38 +53,30 @@ const About = () => {
           style={{
             padding: '20px',
             font: 'lighter',
-            backgroundColor: '#00667a',
+            backgroundColor: 'rgb(106 49 31)',
+            textAlign: 'center',
           }}
         >
-          I'm a <strong>full-stack web developer</strong> <br />
-          coming from the background <br />
-          of a professional musician <br />
-          and stand-up comedian. <br />
-          But these things aren't <br />
-          all that <br />
-          different. <br />
-          Being a <strong>musician</strong> <br />
+          I'm a <strong>full-stack web developer</strong>
+          coming from the background of a professional musician and stand-up
+          comedian. But these things aren't all that different. Being a{' '}
+          <strong>musician</strong>
           and <strong>comedian</strong>
-          <br /> means developing a relationship <br />
-          with the <strong>audience</strong> <br />
-          and making my taste
-          <br /> in music <br />
-          and jokes fit <br />
-          with what <strong>THEY</strong> want. <br />
-          It also means lots of experience <br />
-          working on <strong>teams</strong> <br />
-          and with other <strong>people</strong> <br />
-          in real world settings. <br />
-          As a <strong>full-stack web developer</strong> <br />I relish the
-          <strong> opportunities</strong> <br />
-          to figure out <br />
-          what <strong>clients</strong> want, <br />
-          and implementing that <strong>efficiently</strong>, <br />
-          effectively, <br />
-          and with a little bit of <strong>humor</strong>
-          <br /> and <strong>music</strong> <br />
-          thrown in where possible. <br />
-          (Photo credit: Daniel Solano)
+          means developing a relationship with the <strong>audience</strong>
+          and making my taste in music and jokes fit with what{' '}
+          <strong>THEY</strong> want. It also means lots of experience working
+          on <strong>teams</strong>
+          and with other <strong>people</strong>
+          in real world settings. As a <strong>
+            full-stack web developer
+          </strong>{' '}
+          I relish the
+          <strong> opportunities</strong>
+          to figure out what <strong>clients</strong> want, and implementing
+          that <strong>efficiently</strong>, effectively, and with a little bit
+          of <strong>humor</strong>
+          and <strong>music</strong>
+          thrown in where possible. (Photo credit: Daniel Solano)
         </p>
       </div>
     </div>
