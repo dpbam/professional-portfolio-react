3268135086b986302a192762751cd91c4c6c69b3
dpbam <derekphtpt@gmail.com>
2021-11-06 13:13:45 -0600

    mobile view words running together. fixed that

--- src/components/About.js
+++ src/components/About.js
@@ -57,22 +57,18 @@ const About = () => {
             textAlign: 'center',
           }}
         >
-          I'm a <b>front-end web developer</b>
-          coming from the background of a professional musician and stand-up
-          comedian. But these things aren't all that different. Being a{' '}
-          <b>musician</b>
-          and <b>comedian</b>
-          means developing a relationship with the <b>audience</b>
-          and making my taste in music and jokes fit with what <b>THEY</b> want.
-          It also means lots of experience working on <b>teams</b>
-          and with other <b>people</b>
-          in real world settings. As a <b>front-end web developer</b> I relish
-          the
-          <b> opportunities</b>
-          to figure out what <b>clients</b> want, and implementing that{' '}
-          <b>efficiently</b>, effectively, and with a little bit of <b>humor</b>
-          and <b>music</b>
-          thrown in where possible. (Photo credit: Daniel Solano)
+          I'm a <b>front-end web developer</b> coming from the background of a
+          professional musician and stand-up comedian. But these things aren't
+          all that different. Being a <b>musician</b> and <b>comedian</b> means
+          developing a relationship with the <b>audience</b> and making my taste
+          in music and jokes fit with what <b>THEY</b> want. It also means lots
+          of experience working on <b>teams</b> and with other <b>people</b> in
+          real world settings. As a <b>front-end web developer</b> I relish the{' '}
+          <b> opportunities</b> to figure out what <b>clients</b> want, and
+          implementing that <b>efficiently</b>, effectively, and with a little
+          bit of <b>humor</b>
+          and <b>music</b> thrown in where possible. (Photo credit: Daniel
+          Solano)
         </p>
       </div>
     </div>
